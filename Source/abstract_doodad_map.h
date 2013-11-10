27c9c5508e6ea90c7a724852f61f5cb85c2e8ccb
evinstk <tanner.evins@gmail.com>
2013-11-10 15:31:40 -0600

    'Source' to 'src'

--- Source/abstract_doodad_map.h
+++
@@ -1,7 +0,0 @@
-#ifndef IDOODAD_MAP
-#define IDOODAD_MAP
-
-class AbstractDoodadMap {
-};
-
-#endif /* IDOODAD_MAP */
--- Source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "simple_background_map.h"
-#include "SDL/SDL.h"
-
-int main(int argc, char *args[]) {
-	SimpleBackgroundMap(Background::Grass, 250, 250);
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Quit();
-	return 0;
-}
